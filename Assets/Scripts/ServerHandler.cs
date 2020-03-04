6946919f11e8f4dbf3b7797f61fcc2a40b1511c4
Manan Joshi <mananjoshi1995@gmail.com>
2020-03-03 19:54:13 -0500

    Adding TextMeshPro package and showing dummy text on the screen

--- Assets/Scripts/ServerHandler.cs
+++ Assets/Scripts/ServerHandler.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using SocketIO;
+using TMPro;
 
 public class ServerHandler : MonoBehaviour
 {
-  public GameObject Cube;
+  private GameObject Screen;
   SocketIOComponent socket;
-  // Start is called before the first frame update
   bool active = false;
+  private Queue<string> MessageQueue;
+  private TextMesh TextElement;
+
+  private void Awake()
+  {
+    Screen = GetComponent<GameObject>();
+  }
+
+  // Start is called before the first frame update
   void Start()
   {
     socket = GameObject.Find("SocketIO").GetComponent<SocketIOComponent>();
-    Cube.SetActive(active);
+    MessageQueue = new Queue<string>();
     socket.On("message", onMessageEvent);
+    TextElement = Screen.GetComponentInChildren<Canvas>().GetComponentInChildren<TextMesh>();
   }
 
   // Update is called once per frame
@@ -25,7 +35,17 @@ public class ServerHandler : MonoBehaviour
   void onMessageEvent(SocketIOEvent evt)
   {
     Debug.Log("Message: " + evt.data.GetField("message"));
-    Cube.SetActive(active);
-    active = !active;
+    MessageQueue.Enqueue(evt.data.GetField("message").ToString());
+    // active = !active;
+    Debug.Log("Queue: " + MessageQueue.Count + "TextElement: " + TextElement);
+    displayText();
+  }
+
+  void displayText()
+  {
+    while (MessageQueue.Count > 0)
+    {
+      // TextElement.text += (MessageQueue.Dequeue() + " ");
+    }
   }
 }
